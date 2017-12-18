3c96d9e55061363dc05adaeec3d1e031d107fbe0
Thodoris <tpapak@gmail.com>
2017-12-18 20:42:12 +0100

    streamstatistics test dependencies

--- tests/streamstats.R
+++ tests/streamstats.R
@@ -3,10 +3,12 @@ rm(list=ls())
 
 library("devtools")
 install_github("esm-ispm-unibe-ch/flow_contribution")
+install_github("esm-ispm-unibe-ch/dataformatter")
 install_github("esm-ispm-unibe-ch/nmadata")
 
 library(contribution)
 library(nmadata)
+library(dataformatter)
 
 #indata = read.csv("long_conti.csv",header=TRUE,sep=";")
 #C = getHatMatrix(indata,type="long_continuous",model="random",sm="MD")
@@ -15,17 +17,12 @@ library(nmadata)
 #indata = wide2long(wideData,"binary")
 #C = getHatMatrix(indata,type="long_binary",model="random",sm="OR")
 
-
 data(cipriani_2011)
 wideData = cipriani_2011
 indata = wide2long(wideData,"binary")
 
 
-
-
-
 C = getHatMatrix(indata,type="long_binary",model="random",sm="OR")
-
-
  
 cl = streamStatistics(C)
+plot(cl$cummulativeContributionPerStream)
